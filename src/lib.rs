05a5c577ebfcb3256d2ab505de60f390f1c4f17b
Edgar <edgar21_9@hotmail.com>
2022-11-20 15:21:09 -0600

    Applied fmt

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use jwalk::{WalkDirGeneric};
+use jwalk::WalkDirGeneric;
 
 pub struct FileFinderConfig<'a> {
     // Directory to walk, if `None` defaults to `process.cwd`
@@ -15,9 +15,13 @@ pub struct FileFinderConfig<'a> {
     pub err_behavior: ErrorBehavior,
 }
 
+#[derive(Debug)]
 pub enum ErrorBehavior {
+    // In case of Err Result will continue execution
     Ignore,
+    // Will panic in case an Err Result is found during execution
     Panic,
+    // In case of Err Result will continue execution and will log the error message
     Log,
 }
 
@@ -31,23 +35,28 @@ pub fn find_files<'a>(config: FileFinderConfig<'a>) -> Vec<PathBuf> {
 
     let walk_dir = WalkDirGeneric::<((i32), (bool))>::new(dir_to_scan).process_read_dir(
         move |_, _, _, children| {
-
             // Apply skip
             children
                 .iter_mut()
                 .for_each(|dir_entry_result| match dir_entry_result {
                     Ok(dir_entry) => {
-            
-
                         if let Some(skipped) = config.skipped_dirs {
                             if dir_entry.file_type.is_dir() && skipped(dir_entry.path().as_path()) {
                                 dir_entry.read_children_path = None;
                             }
                         }
                     }
-                    Err(_) => {}
+                    Err(e) => match &config.err_behavior {
+                        ErrorBehavior::Ignore => {}
+                        ErrorBehavior::Panic => {
+                            panic!("Error during Apply Skip operation {}", e);
+                        }
+                        ErrorBehavior::Log => {
+                            println!("Error during Apply Skip operation {}", e);
+                        }
+                    },
                 });
-            
+
             // Apply filter
             children.retain(|dir_entry_result| {
                 match dir_entry_result {
@@ -58,12 +67,18 @@ pub fn find_files<'a>(config: FileFinderConfig<'a>) -> Vec<PathBuf> {
                             }
                         }
                     }
-                    Err(_) => {}
+                    Err(e) => match &config.err_behavior {
+                        ErrorBehavior::Ignore => {}
+                        ErrorBehavior::Panic => {
+                            panic!("Error during Apply filter operation {}", e);
+                        }
+                        ErrorBehavior::Log => {
+                            println!("Error during Apply filter operation {}", e);
+                        }
+                    },
                 }
                 true
             });
-
-            
         },
     );
 
@@ -76,8 +91,8 @@ pub fn find_files<'a>(config: FileFinderConfig<'a>) -> Vec<PathBuf> {
                         return Some(e.path());
                     }
                     None
-                },
-                Err(_) => None,
+                }
+                Err(e) => None,
             }
         })
         .collect()
@@ -85,7 +100,7 @@ pub fn find_files<'a>(config: FileFinderConfig<'a>) -> Vec<PathBuf> {
 
 #[cfg(test)]
 mod tests {
-    use std::{env, path::{Path, PathBuf}, str::FromStr};
+    use std::{env, path::Path};
 
     use crate::{find_files, FileFinderConfig};
 
@@ -106,9 +121,7 @@ mod tests {
         env::set_current_dir(Path::new("./test_folder")).ok();
         let found_files = find_files(FileFinderConfig {
             dir: None,
-            filter: Some(|path| {
-                path.ends_with(".js")
-            }),
+            filter: Some(|path| path.ends_with(".js")),
             skipped_dirs: None,
             err_behavior: crate::ErrorBehavior::Ignore,
         });
@@ -123,12 +136,14 @@ mod tests {
             filter: None,
             skipped_dirs: Some(|path| {
                 if path.ends_with("this_is_not_scanned") {
-                    return true
+                    return true;
                 }
                 false
             }),
             err_behavior: crate::ErrorBehavior::Ignore,
         });
-        assert!(!found_files.iter().any(|f| f.ends_with("not_found_sad_face.js")));
+        assert!(!found_files
+            .iter()
+            .any(|f| f.ends_with("not_found_sad_face.js")));
     }
 }
